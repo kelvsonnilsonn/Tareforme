0a72a516cb706dc48cc71ff6086f0eaf010350a3
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-08-24 14:48:11 -0300

    feat: add TaskInfo value object composing Name and Description

---
+++ src/main/java/com/github/tareforme/domain/valueobjects/TaskInfo.java
@@ -0,0 +1,27 @@
+package com.github.tareforme.domain.valueobjects;
+
+import com.github.tareforme.domain.expeptions.InvalidNameException;
+import jakarta.persistence.Embedded;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TaskInfo {
+    @Embedded
+    private Name name;
+    @Embedded
+    private Description description;
+
+    public TaskInfo(String name, String description){
+        this.name = new Name(name);
+        this.description = new Description(description);
+    }
+
+    public TaskInfo ofName(String oName) throws InvalidNameException {
+       return new TaskInfo(oName, this.description.getDescription());
+    }
+
+    public TaskInfo ofDescription(String oDescription){
+        return new TaskInfo(this.name.getName(), oDescription);
+    }
+}
