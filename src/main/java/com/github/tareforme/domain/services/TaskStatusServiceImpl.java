d06e857ca77faf19cb2210be1989ea6a3a134a21
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-08-29 23:07:45 -0300

    fix: add TaskRepository to remove circular dependency with TaskService.
    - add save method to update tasks in database.

--- src/main/java/com/github/tareforme/domain/services/TaskStatusServiceImpl.java
+++ src/main/java/com/github/tareforme/domain/services/TaskStatusServiceImpl.java
@@ -4,10 +4,15 @@ import com.github.tareforme.domain.expeptions.InvalidTaskTransitionException;
 import com.github.tareforme.domain.model.Task;
 import com.github.tareforme.domain.model.TaskStatus;
 import com.github.tareforme.domain.ports.service.TaskStatusService;
+import com.github.tareforme.infra.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskStatusServiceImpl implements TaskStatusService {
+
+    @Autowired
+    private TaskRepository taskDAO;
 
     @Override
     public boolean setPedingStatus(Task task) {
@@ -15,6 +20,7 @@ public class TaskStatusServiceImpl implements TaskStatusService {
             throw new InvalidTaskTransitionException("A task já está em andamento.");
         }
         task.setStatus(TaskStatus.PENDING);
+        taskDAO.save(task);
         return true;
     }
 
@@ -27,6 +33,7 @@ public class TaskStatusServiceImpl implements TaskStatusService {
             throw new InvalidTaskTransitionException("A task precisa estar em andamento para ser completada.");
         }
         task.setStatus(TaskStatus.COMPLETED);
+        taskDAO.save(task);
         return true;
     }
 }
