e0178f58dcae079b9f72aa0ab95b3c1d03370f76
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-06 11:18:07 -0300

    fix: User package was incorrect.

--- src/main/java/com/github/tareforme/domain/ports/service/UserService.java
+++ src/main/java/com/github/tareforme/domain/ports/service/UserService.java
@@ -4,10 +4,13 @@ import com.github.tareforme.domain.expeptions.InvalidNameException;
 import com.github.tareforme.domain.expeptions.InvalidPasswordException;
 import com.github.tareforme.domain.model.User;
 
+import java.util.List;
+
 public interface UserService {
     void create(String name, String pass) throws InvalidNameException, InvalidPasswordException;
     void delete(User user);
     void delete(Long id);
     void update(User user);
+    List<User> findAll();
     User findById(Long id);
 }
