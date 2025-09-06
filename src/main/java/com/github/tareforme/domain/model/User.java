6bb94854e0dcdcfe4e9538eef57a4b2c27b638ac
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-06 11:19:13 -0300

    fix: remove abstract modifier and turned DefaultUser only in a User entity.

--- src/main/java/com/github/tareforme/domain/model/User.java
+++ src/main/java/com/github/tareforme/domain/model/User.java
@@ -1,16 +1,54 @@
 package com.github.tareforme.domain.model;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import com.github.tareforme.domain.expeptions.InvalidNameException;
+import com.github.tareforme.domain.expeptions.InvalidPasswordException;
+import com.github.tareforme.domain.valueobjects.Name;
+import com.github.tareforme.domain.valueobjects.Password;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Set;
+
+@Setter
+@Getter
 @Entity
-@DiscriminatorValue("USER")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public final class User extends DefaultUser {
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    @Embedded
+    private Password password;
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER, orphanRemoval = true)
+    private Set<Task> tasks;
+
+    protected User() {};
 
     public User(String name, String pass){
-        super(name, pass);
+        this.name = new Name(name);
+        this.password = new Password(pass);
+    }
+
+    public void changePassword(String oPassword) throws InvalidPasswordException {
+        this.password = Password.of(oPassword);
+    }
+
+    public void changeName(String oName) throws InvalidNameException {
+        this.name = Name.of(oName);
+    }
+
+    public String getName(){
+        return this.name.getName();
+    }
+
+    public String getPassword(){
+        return this.password.getPassword();
     }
 }
