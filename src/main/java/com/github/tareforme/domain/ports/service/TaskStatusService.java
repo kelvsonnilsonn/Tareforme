31a3caccf672ec0c04a9c13fd7778aa51fe8c6a6
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-06 11:17:07 -0300

    fix: User package was incorrect.

--- src/main/java/com/github/tareforme/domain/ports/service/TaskStatusService.java
+++ src/main/java/com/github/tareforme/domain/ports/service/TaskStatusService.java
@@ -3,6 +3,6 @@ package com.github.tareforme.domain.ports.service;
 import com.github.tareforme.domain.model.Task;
 
 public interface TaskStatusService {
-    void setPedingStatus(Task task);
-    void setCompleteStatus(Task task);
+    void setPedingStatus(Long taskId);
+    void setCompleteStatus(Long taskId);
 }
--- src/main/java/com/github/tareforme/infra/repositories/TaskRepository.java
+++ src/main/java/com/github/tareforme/infra/repositories/TaskRepository.java
@@ -1,7 +1,15 @@
 package com.github.tareforme.infra.repositories;
 
+import com.github.tareforme.domain.model.User;
 import com.github.tareforme.domain.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByOwner(User owner);
+    Page<Task> findByOwner(User owner, Pageable pageable);
 }
